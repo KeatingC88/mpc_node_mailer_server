a1cb37d42bfa36526d5abb93c56a7ac50424618a
keatingc88@gmail.com <keatingc88@gmail.com>
2025-01-24 15:17:25 -0600

    updated with get ip address from only the ethernet protocol

--- mpc_node_mailer_server.js
+++ mpc_node_mailer_server.js
@@ -1,16 +1,18 @@
 `use strict`
 require('dotenv').config()
+const os = require("os")
+const os_cpu_count = os.cpus().length
 const express = require(`express`)
 const cluster = require(`node:cluster`)
-const os_cpu_count = require("os").cpus().length
 const bcrypt = require(`bcrypt`)//must be uninstalled locally before putting into a docker container (so docker installs from inside itself).
 const nodemailer = require(`nodemailer`)
 const crypto = require('crypto')
 
 const client_address = process.env.CLIENT_ADDRESS_DEV
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY
-const network_socket_port = process.env.SERVER_PORT | 3001
-const network_ip_address = `192.168.0.102`
+
+let network_socket_port = process.env.SERVER_PORT | 3001
+let network_ip_address = ``
 
 // Encrypt function for AES-128 (16-byte key)
 const Encrypt = (value) => {
@@ -28,117 +30,137 @@ const Decrypt = (value) => {
     return decrypted;
 }
 
-if (cluster.isPrimary) {
-    for (let i = 0; i < os_cpu_count; i++) {
-        cluster.fork()
+const local_ip_address = () => {
+    const networkInterfaces = os.networkInterfaces()
+    for (const interfaceName in networkInterfaces) {
+        if (interfaceName.toLowerCase().includes('eth') || interfaceName.toLowerCase() === 'ethernet') {
+            for (const networkInterface of networkInterfaces[interfaceName]) {
+                if (networkInterface.family === 'IPv4' && !networkInterface.internal) {
+                    return networkInterface.address
+                }
+            }
+        }
     }
-    cluster.on('exit', (worker, code, signal) => {
-        cluster.fork()
-    })
-} else {
-    const app = express()
-    app.use(express.static('client/build'))
-    app.use(express.json())
-    
-    app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*")
-        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-        next()
-    })
+    return null
+}
 
-    app.get('*', async (req, res) => {
-        await res.send(JSON.stringify(`Server Ready...`))
-    })
+try {
+    network_ip_address = `${local_ip_address()}`
 
-    app.post("/api/Send/Confirmation/Email/", async (req, res) => {
-        let email_address = await Decrypt(req.body.email_address)
-        let language = await Decrypt(req.body.language)
-        let region = await Decrypt(req.body.region)
+    if (cluster.isPrimary) {
+        for (let i = 0; i < os_cpu_count; i++) {
+            cluster.fork()
+        }
+        cluster.on('exit', (worker, code, signal) => {
+            cluster.fork()
+        })
+    } else {
+        const app = express()
+        app.use(express.static('client/build'))
+        app.use(express.json())
 
-        let verification_access_code = await bcrypt.hashSync(email_address, 16)
-
-        do {
-            if (verification_access_code.charAt(verification_access_code.length - 1) === ".") {
-                verification_access_code = await bcrypt.hashSync(email_address, 16);
-            }
-        } while (verification_access_code.charAt(verification_access_code.length - 1) === ".");
-
-        const transporter = await nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: `${process.env.NODE_MAILER_USER}`,
-                pass: `${process.env.NODE_MAILER_PASSWORD}`
-            }
+        app.use(function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*")
+            res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+            next()
         })
 
-        await transporter.sendMail({
-            from: `${process.env.NODE_MAILER_USER}`,
-            to: `${email_address}`,
-            subject: 'MPC Account Registration Email',
-            text: `Confirmation Link: ${client_address}/password?language=${language}-${region}&email=${email_address}&code=${verification_access_code}`
-        }, (error, info) => {
-            if (error) {
-                res.setHeader("Content-Type", "application/json")
-                res.status(500)
-                res.json({ error: `unable to send email.`})
-            }
+        app.get('*', async (req, res) => {
+            await res.send(JSON.stringify(`Server Ready...`))
         })
 
-        res.setHeader("Content-Type", "application/json")
-        res.status(200)
-        res.json({code: `${verification_access_code}`})
-    })
+        app.post("/api/Send/Confirmation/Email/", async (req, res) => {
+            let email_address = await Decrypt(req.body.email_address)
+            let language = await Decrypt(req.body.language)
+            let region = await Decrypt(req.body.region)
 
-    app.post("/api/Send/Notification/Email/", async (req, res) => {
-        let email_address = await Decrypt(req.body.email_address)
-        let language = await Decrypt(req.body.language)
-        let region = await Decrypt(req.body.region)
-        const ip_address = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-        let client_time = await Decrypt(req.body.client_time)
-        let location = await Decrypt(req.body.location)
+            let verification_access_code = await bcrypt.hashSync(email_address, 16)
 
-        const transporter = await nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: `${process.env.NODE_MAILER_USER}`,
-                pass: `${process.env.NODE_MAILER_PASSWORD}`
-            }
+            do {
+                if (verification_access_code.charAt(verification_access_code.length - 1) === ".") {
+                    verification_access_code = await bcrypt.hashSync(email_address, 16);
+                }
+            } while (verification_access_code.charAt(verification_access_code.length - 1) === ".");
+
+            const transporter = await nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: `${process.env.NODE_MAILER_USER}`,
+                    pass: `${process.env.NODE_MAILER_PASSWORD}`
+                }
+            })
+
+            await transporter.sendMail({
+                from: `${process.env.NODE_MAILER_USER}`,
+                to: `${email_address}`,
+                subject: 'MPC Account Registration Email',
+                text: `Confirmation Link: ${client_address}/password?language=${language}-${region}&email=${email_address}&code=${verification_access_code}`
+            }, (error, info) => {
+                if (error) {
+                    res.setHeader("Content-Type", "application/json")
+                    res.status(500)
+                    res.json({ error: `unable to send email.` })
+                }
+            })
+
+            res.setHeader("Content-Type", "application/json")
+            res.status(200)
+            res.json({ code: `${verification_access_code}` })
         })
 
-        await transporter.sendMail({
-            from: `${process.env.NODE_MAILER_USER}`,
-            to: `${email_address}`,
-            subject: 'MPC Account Registration Attempt',
-            text: `There's been an attempt to re-register your email account from 
-            \nIP Address: ${ip_address}
-            \nLanguage: ${language}
-            \nRegion: ${region}
-            \nDevice Time Captured: ${new Date(parseInt(client_time)).toISOString() }
-            \nLocation: ${location}
-            \nNotify an MPC Admin if the issue persists and was NOT you.`
-        }, (error) => {
-            if (error) {
-                res.setHeader("Content-Type", "application/json")
-                res.status(500)
-                res.json(false)
-            }
-        })
-        res.status(200)
-        res.json(true)
-    })
+        app.post("/api/Send/Notification/Email/", async (req, res) => {
+            let email_address = await Decrypt(req.body.email_address)
+            let language = await Decrypt(req.body.language)
+            let region = await Decrypt(req.body.region)
+            const ip_address = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+            let client_time = await Decrypt(req.body.client_time)
+            let location = await Decrypt(req.body.location)
 
-    app.post("/api/Received/Confirmation/Email/", async (req, res) => {
-        res.setHeader("Content-Type", "application/json")
-        let email_address = await Decrypt(req.body.email_address)
-        if (!bcrypt.compareSync(email_address, req.body.code)) {
-            res.status(401)
-            res.json(false)
-        } else {
+            const transporter = await nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: `${process.env.NODE_MAILER_USER}`,
+                    pass: `${process.env.NODE_MAILER_PASSWORD}`
+                }
+            })
+
+            await transporter.sendMail({
+                from: `${process.env.NODE_MAILER_USER}`,
+                to: `${email_address}`,
+                subject: 'MPC Account Registration Attempt',
+                text: `There's been an attempt to re-register your email account from 
+                        \nIP Address: ${ip_address}
+                        \nLanguage: ${language}
+                        \nRegion: ${region}
+                        \nDevice Time Captured: ${new Date(parseInt(client_time)).toISOString()}
+                        \nLocation: ${location}
+                        \nNotify an Admin if the issue persists and was NOT you at {website}.`
+            }, (error) => {
+                if (error) {
+                    res.setHeader("Content-Type", "application/json")
+                    res.status(500)
+                    res.json(false)
+                }
+            })
             res.status(200)
             res.json(true)
-        }
-    })
-    
-    app.listen(network_socket_port, network_ip_address, () => console.log(`Node Mailer Server:\na CPU Core is listening on \nNetwork IP Address ${network_ip_address} \nNetwork Socket PORT ${network_socket_port}\n`))
+        })
+
+        app.post("/api/Received/Confirmation/Email/", async (req, res) => {
+            res.setHeader("Content-Type", "application/json")
+            let email_address = await Decrypt(req.body.email_address)
+            if (!bcrypt.compareSync(email_address, req.body.code)) {
+                res.status(401)
+                res.json(false)
+            } else {
+                res.status(200)
+                res.json(true)
+            }
+        })
+
+        app.listen(network_socket_port, network_ip_address, () => console.log(`Node Mailer Server:\na CPU Core is listening on \nNetwork IP Address ${network_ip_address} \nNetwork Socket PORT ${network_socket_port}\n`))
+    }
+} catch (err) {
+    console.error('Error:', err)
 }
