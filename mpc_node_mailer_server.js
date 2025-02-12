4be6405cd837fd2e34484bbb74c59fcf51dd4da6
keatingc88@gmail.com <keatingc88@gmail.com>
2025-02-12 11:28:35 -0600

    testing env and not

--- mpc_node_mailer_server.js
+++ mpc_node_mailer_server.js
@@ -9,11 +9,14 @@ const nodemailer = require(`nodemailer`)
 const crypto = require('crypto')
 
 //const client_address = process.env.CLIENT_ADDRESS_DEV
-const client_address = process.env.CLIENT_ADDRESS_192_168_0_102_6499
-const encryption_key = process.env.ENCRYPTION_KEY
-const network_socket_port = process.env.SERVER_PORT
-const network_ip_address = process.env.SERVER_ADDRESS_192_168_0_102
+//const client_address = process.env.CLIENT_ADDRESS_192_168_0_102_6499
+//const encryption_key = process.env.ENCRYPTION_KEY
+//const network_socket_port = process.env.SERVER_PORT
+//const network_ip_address = process.env.SERVER_ADDRESS_192_168_0_102
 //const network_ip_address = process.env.SERVER_ADDRESS_DEV
+//const network_socket_port = 3001
+//const network_ip_address = `192.168.0.102`
+//const network_ip_address = `127.0.0.102`
 
 // Encrypt function for AES-128 (16-byte key)
 const Encrypt = (value) => {
